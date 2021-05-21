5297272fb8a8ac87f6d44bb54d16a865daeb569b
dhr231 <dhruvprashar231@gmail.com>
2021-05-22 01:04:25 +0530

    Calculate employee wage for different companies.

--- main/java/com/assignment/employeeWage.java
+++ main/java/com/assignment/employeeWage.java
@@ -1,18 +1,28 @@
 package com.assignment;
 
 public class employeeWage {
-    public static final int IS_FULL_TIME = 2;
-    public static final int DAYS_PER_MONTH = 20;
     public static final int IS_PART_TIME = 1;
-    public static final int TOTAL_WORK_HOURS = 100;
-    public static final int WAGE_PER_HOUR = 20; // public static because they are to be used in the entire code and
-    // final because the values are constant
+    public static final int IS_FULL_TIME=2;
 
-    public static int calculateWage(){
+    private final String company;
+    private final int DAYS_PER_MONTH;
+    private final int TOTAL_WORK_HOURS;
+    private final int WAGE_PER_HOUR;
+    public employeeWage(String company, int DAYS_PER_MONTH, int TOTAL_WORK_HOURS, int WAGE_PER_HOUR){
+        this.company=company;
+        this.DAYS_PER_MONTH=DAYS_PER_MONTH;
+        this.TOTAL_WORK_HOURS=TOTAL_WORK_HOURS;
+        this.WAGE_PER_HOUR=WAGE_PER_HOUR;
+    }
+
+
+    public static void main(String args[]) {
+        employeeWage dMart=new employeeWage("DMART",20,8,100);
+        employeeWage reliance=new employeeWage("RELIANCE",25,6,80);
         int emp_monthly_wage = 0;
         int totalHours = 0;
         int totalDays = 0;
-        while (totalHours < TOTAL_WORK_HOURS && totalDays < DAYS_PER_MONTH) {
+        while (totalHours < dMart.TOTAL_WORK_HOURS && totalDays < dMart.DAYS_PER_MONTH) {
             int emp_hours = 0;
             int emp_daily_wage = 0;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -26,20 +36,13 @@ public class employeeWage {
                 default:
                     emp_hours = 0;
             }
-            emp_daily_wage = emp_hours * WAGE_PER_HOUR;
+            emp_daily_wage = emp_hours * dMart.WAGE_PER_HOUR;
             emp_monthly_wage += emp_daily_wage;
             totalDays++;
             totalHours++;
         }
 
         System.out.println("ëmployee wage of the month is :" + emp_monthly_wage);
-        return emp_monthly_wage;
-    }
-    public static void main(String args[]) {
-        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-        calculateWage();
-
-
 
     }
 }
